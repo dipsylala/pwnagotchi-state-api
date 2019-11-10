afb9bb8a0709538694c6bb4fab37ac20ece64342
Dispsylala <marcus.watson@loumiaconsulting.com>
2019-11-10 21:16:50 +0000

    Themes moved to a subdirectory for easier management. state.html renamed state-default.html to be more obvious it's the default

--- state-api.py
+++ state-api.py
@@ -27,12 +27,12 @@ class StateApi(plugins.Plugin):
             return jsonify({"message": "Method Not Allowed"}), 405
 
         if path == 'display':
-            theme = "state.html"
+            theme = "state-default.html"
 
             if 'theme' in self.options:
                 theme = "state-" + self.options["theme"] + ".html"
 
-            return Response(Path(os.path.dirname(os.path.realpath(__file__)) + '/' + theme).read_text(), 'text/html')
+            return Response(Path(os.path.dirname(os.path.realpath(__file__)) + '/state-api/' + theme).read_text(), 'text/html')
 
         if self.DISPLAY is None:
             return jsonify({"initialised": "false"})
